591e549c317cbb16303fec927ca0b55e60f78167
Snail26 <141910498+Snail26@users.noreply.github.com>
2024-02-11 15:00:05 +1300

    updates
    added room listening from any room

--- patch.js
+++ patch.js
@@ -21,6 +21,41 @@ socket._callbacks["$getNames"][0] = socket._callbacks.$getNames.pop();
 
 socket.emit("getNames");
 
-socket.on('chat message', socket._callbacks["$chat message"].toString().replace("if(room1 == room)", "if(room1)").replace("...(finalContent.reverse())", "...(room1 + finalContent.reverse())"));
+socket.on("chat message", (msg, room1) => {
+        if (focused == false) {
+          playSound(localStorage.notifacationSound);
+        }
+        var finalContent = [];
+        msg.split(" ").forEach((v, i) => {
+            if(i) {
+                if(typeof (finalContent[0]) == "string") {
+                    finalContent[0] += " ";
+                } else finalContent.unshift(" ");
+            }
+            // https://stackoverflow.com/a/3809435
+            if(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(v)) {
+                var el = document.createElement("a");
+                el.href = v;
+                el.textContent = v;
+                el.style.textDecoration = "underline";
+                el.style.color = "blue";
+                el.style.cursor = "pointer";
+                finalContent.unshift(el);
+            } else {
+                if(i) {
+                    finalContent[0] += v;
+                } else finalContent.unshift(v);
+            }
+        });
+        var item = document.createElement('li');
+        item.append(...("[{from room: " + room1 + "}]: " + finalContent.reverse()));
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+        item.setAttribute("class", brightness)
+});
+
+socket._callbacks['$chat message'][0] = socket._callbacks['$chat message'].pop();
+
+socket.on()
 
 change();
